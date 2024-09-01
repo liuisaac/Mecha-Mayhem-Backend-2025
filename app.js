2bcd64cfc28eef0bb1902a3b11c4e6205c6f5713
liuisaac <80558145+liuisaac@users.noreply.github.com>
2024-08-31 21:18:49 -0600

    updaed net interfaces to not restrict access

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.use('/matches', matchRouter)
 module.exports = app;
 
 const PORT = process.env.DEV_PORT;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
